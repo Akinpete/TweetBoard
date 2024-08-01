fec84bc6f2fc3e38f0e21c77504f3bc2e63dd600
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-08-01 14:56:20 +0100

    code restructuring with more features

--- start_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export HBNB_MYSQL_USER=tweetbox_dev
-export HBNB_MYSQL_PWD=tweetbox_dev_pwd
-export HBNB_MYSQL_HOST=localhost
-export HBNB_MYSQL_DB=tweetbox_dev_db
-
-
-python3 -m web_flask.app
